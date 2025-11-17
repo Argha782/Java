9dbd10572416118b32dea56dd2b68e4ad8bdca39
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-11-17 23:03:24 +0530

    Data types

---
+++ Data_Types.java
@@ -0,0 +1,34 @@
+public class Data_Types {
+public static void main(String a[])
+    {
+        int num1 = 5;
+        byte by = 9; 
+        short sh= 256;
+        long l= 512l;
+
+        float f=85.678f;
+        double d= 34.655647835634756;
+
+        char c= 'k';
+
+        boolean b= true;
+
+        int num2 = 6;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+
+        int bin = 0b101;
+        int hex = 0x7E;
+        System.out.println(bin);
+        System.out.println(hex);
+
+    }
+}
