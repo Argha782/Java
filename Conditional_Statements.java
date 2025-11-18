49e057c25d85e54262793c8a5aee3d82f7f1cea6
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-11-18 13:56:26 +0530

    Add Conditional Statements example with If-Else, Ternary, and Switch

---
+++ Conditional_Statements.java
@@ -0,0 +1,46 @@
+public class Conditional_Statements {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // If-Else Statement
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        //Ternary Operator
+        String result = (number > 0) ? "Positive" : "Non-positive";
+        System.out.println(number + " is " + result);
+
+        // Switch Statement
+        int day = 3;
+        String dayName;
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+        }
+        System.out.println("Day " + day + " is " + dayName);
+    }
+}
