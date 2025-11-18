13b8c87965e6170ec08f2c86b372bbba772db570
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-11-18 13:55:17 +0530

    Arithmetic Operators

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,18 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        int quot = a / b;
+        int mod = a % b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + diff);
+        System.out.println("Product: " + prod);
+        System.out.println("Quotient: " + quot);
+        System.out.println("Modulus: " + mod);
+    }
+}
